a68022a54767420fa47fec99bb74e818d5eef2c2
chandan-002 <chandan.unicoglobal@gmail.com>
2022-11-18 22:08:12 +0530

    pus

---
+++ src/config.js
@@ -0,0 +1,7 @@
+/* eslint-disable prettier/prettier */
+
+// export const SOCKET_IO_SERVER = 'https://rtmp-live-test-eyse.herokuapp.com';
+// export const RTMP_SERVER = 'rtmp://rtmp-live-test-eyse.herokuapp.com:1935';
+
+export const SOCKET_IO_SERVER = 'http://192.168.1.9:3333';
+export const RTMP_SERVER = 'rtmp://192.168.1.9';
